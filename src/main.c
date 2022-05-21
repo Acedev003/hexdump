9ed8b7ff3abdedb96fecfb8e80eec1ed19d7c8f4
Acedev003 <acedeveloper003@gmail.com>
2022-05-21 20:35:39 +0530

    Refactored header file location

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-
-#include "../include/hexdump.h"
+#include "hexdump.h"
 
 #define HEXDUMP_VERSION "0.0.1"
 
 int main(int argc,char* argv[])
 {
     printf("Hexdump:v%s \n\n",HEXDUMP_VERSION);
+    
     print_hex(argv[argc-1]);
 
     return 0;
