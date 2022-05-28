e2f0822a6032263eeecc4383ae725c9e5b4227c6
Acedev003 <acedeveloper003@gmail.com>
2022-05-28 23:59:12 +0530

    Fixed bugs and implement start pos, no of bytes

--- src/hexdump.c
+++ src/hexdump.c
@@ -3,154 +3,192 @@
 #include<sys/stat.h>
 #include"hexdump.h"
 
-#define MIN_LINE_BITS   8
-#define BYTES_PER_LINE  16
-#define LINES_PER_BLOCK 100
-#define BLOCK_SIZE      (MIN_LINE_BITS + 2 + (3*BYTES_PER_LINE) + 2 + 1 + BYTES_PER_LINE + 1 + 1)*LINES_PER_BLOCK + 1
-#define PRINTABLE_MIN   32
-#define PRINTABLE_MAX   126
+#define LINE_COUNTER_WIDTH   8
+#define BYTES_PER_LINE       16
+
+#define PRINTABLE_MIN_ASCII  32
+#define PRINTABLE_MAX_ASCII  126    // https://en.wikipedia.org/wiki/ASCII#Printable_characters
 
 
-int print_file_metadata(char* file_name)
+int print_hex(char *file_name,uint64_t start_byte,uint64_t no_of_bytes)
 {
+    FILE *input_file;
     struct stat file_data;  
+
+    input_file = fopen(file_name,"rb");
+    if(input_file == NULL)
+    {
+        fprintf(stderr,"Error: Failed to open file [%s]\n",file_name);
+        return -1;
+    }
 
     printf("File : %s\n",file_name);
     if(stat(file_name,&file_data) == 0)
     {
         printf("Size : %lld bytes\n\n",file_data.st_size);
     }
+    else
+    {
+        printf("Error: Failed to parse file size");
+        return -1;
+    }
+
+    
     if(file_data.st_size >= UINT64_MAX-1000)
     {
-        fprintf(stderr,"\nError: Too large file\n");
-        return -1;
+        fprintf(stderr,"\nWARN: Too large file. Exiting....\n");
+        return 0;
     }
-    return 0;
-}
 
-
-
-int print_hex(char *file_name,uint64_t start_byte,uint64_t no_of_bytes)
-{
-    FILE *input_file;
-
-    input_file = fopen(file_name,"rb");
-    if(input_file == NULL)
+    if((fseek(input_file,start_byte,SEEK_SET) != 0) || (start_byte > file_data.st_size ))
     {
-        printf("Error: Failed to open file [%s]\n",file_name);
+        fprintf(stderr,"Error: Failed to set start position at byte %lld\n",start_byte);
         return -1;
     }
 
-    int size_check = print_file_metadata(file_name);
-    if(size_check < 0)
+    if(no_of_bytes <= 0)
     {
-        return -1;
+        no_of_bytes = file_data.st_size;
     }
 
-    /*
-    * Scratched my head here for 1 week ..... 
-    * Answer: https://stackoverflow.com/questions/13830338/377-character-in-c#13830379 
-    *         http://c-faq.com/stdio/getcharc.html
-    * 
-    */
 
-    int int_byte; 
+
+    int int_byte;               // Refer NOTES (LL1)
+    
     int line_cursor   = 0;
-    int line_printed = FALSE;
+    int last_line_printed = FALSE;
 
-    uint64_t bytes_read = 0; 
-    uint64_t line_count = 0;
+    uint64_t byte_cursor = ftell(input_file); 
+    uint64_t line_count = byte_cursor / BYTES_PER_LINE;
 
-    unsigned char buffer[3200];
+    unsigned char printf_buffer[3200];
+    unsigned char line[BYTES_PER_LINE];                     // Refer NOTES (LL2)
 
-    /****
-     * 
-     * Another day spent scratching over here 
-     * Solution : https://stackoverflow.com/questions/15823597/use-printf-to-print-character-string-in-hexadecimal-format-distorted-results
-     * 
-     ****/
-    unsigned char line[BYTES_PER_LINE];
+    setvbuf(stdout,printf_buffer,_IOFBF,sizeof(printf_buffer));
 
-    setvbuf(stdout,buffer,_IOFBF,sizeof(buffer));
 
-    while( (int_byte = fgetc(input_file)) != EOF)
+    if(byte_cursor % BYTES_PER_LINE != 0)
+    {
+        int stop_execution = FALSE;
+
+        for(uint64_t i = 0; i < BYTES_PER_LINE; i++)
+        {
+            line[i] = 0;   
+        }
+
+        for(uint64_t i = byte_cursor % BYTES_PER_LINE; i < BYTES_PER_LINE; i++)
+        {
+            line[i] = fgetc(input_file);
+            no_of_bytes--;
+
+            if(line[i] == EOF || no_of_bytes == 0)
+            {
+                stop_execution = TRUE;
+                break;
+            }       
+        }
+
+        printf(
+                "%.*x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+                LINE_COUNTER_WIDTH, line_count,
+                line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],
+                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],
+                (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
+                (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
+                (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
+                (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
+                (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
+                (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
+                (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
+                (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
+                (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
+                (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
+                (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
+                (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
+                (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
+                (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
+                (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
+                (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.'
+                );   
+                line_count++;
+
+                if(stop_execution)
+                {
+                    return 0;
+                }
+    }
+
+    while( (int_byte = fgetc(input_file)) != EOF && no_of_bytes != 0)
     {   
         if( line_cursor == BYTES_PER_LINE )
         {
             printf(
                 "%.*x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
-                MIN_LINE_BITS, line_count,
+                LINE_COUNTER_WIDTH, line_count,
                 line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],
                 line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],
-                (line[ 0] >= PRINTABLE_MIN && line[ 0] <= PRINTABLE_MAX) ? line[ 0] : '.',
-                (line[ 1] >= PRINTABLE_MIN && line[ 1] <= PRINTABLE_MAX) ? line[ 1] : '.',
-                (line[ 2] >= PRINTABLE_MIN && line[ 2] <= PRINTABLE_MAX) ? line[ 2] : '.',
-                (line[ 3] >= PRINTABLE_MIN && line[ 3] <= PRINTABLE_MAX) ? line[ 3] : '.',
-                (line[ 4] >= PRINTABLE_MIN && line[ 4] <= PRINTABLE_MAX) ? line[ 4] : '.',
-                (line[ 5] >= PRINTABLE_MIN && line[ 5] <= PRINTABLE_MAX) ? line[ 5] : '.',
-                (line[ 6] >= PRINTABLE_MIN && line[ 6] <= PRINTABLE_MAX) ? line[ 6] : '.',
-                (line[ 7] >= PRINTABLE_MIN && line[ 7] <= PRINTABLE_MAX) ? line[ 7] : '.',
-                (line[ 8] >= PRINTABLE_MIN && line[ 8] <= PRINTABLE_MAX) ? line[ 8] : '.',
-                (line[ 9] >= PRINTABLE_MIN && line[ 9] <= PRINTABLE_MAX) ? line[ 9] : '.',
-                (line[10] >= PRINTABLE_MIN && line[10] <= PRINTABLE_MAX) ? line[10] : '.',
-                (line[11] >= PRINTABLE_MIN && line[11] <= PRINTABLE_MAX) ? line[11] : '.',
-                (line[12] >= PRINTABLE_MIN && line[12] <= PRINTABLE_MAX) ? line[12] : '.',
-                (line[13] >= PRINTABLE_MIN && line[13] <= PRINTABLE_MAX) ? line[13] : '.',
-                (line[14] >= PRINTABLE_MIN && line[14] <= PRINTABLE_MAX) ? line[14] : '.',
-                (line[15] >= PRINTABLE_MIN && line[15] <= PRINTABLE_MAX) ? line[15] : '.'
+                (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
+                (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
+                (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
+                (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
+                (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
+                (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
+                (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
+                (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
+                (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
+                (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
+                (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
+                (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
+                (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
+                (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
+                (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
+                (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.'
                 );
 
                 line_count  += 16;
                 line_cursor  =  0;
-                line_printed = TRUE;
 
                 line[line_cursor] = int_byte;
+                no_of_bytes--;
                 line_cursor++;
         }
         else
         {
-            line_printed = FALSE;
             line[line_cursor] = int_byte;
+            no_of_bytes--;
             line_cursor++;
         }
 
-        bytes_read++;
+        byte_cursor++;
     }
-    fflush(stdout); 
+    fflush(stdout);
 
-    if(!line_printed)
-    {   
-        int i;
-        for( i = line_cursor; i < BYTES_PER_LINE; i++)
-        {
-            line[i] = 0;       
-        }
-        printf(
-                "%.*x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
-                MIN_LINE_BITS, line_count,
-                line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],
-                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],
-                (line[ 0] >= PRINTABLE_MIN && line[ 0] <= PRINTABLE_MAX) ? line[ 0] : '.',
-                (line[ 1] >= PRINTABLE_MIN && line[ 1] <= PRINTABLE_MAX) ? line[ 1] : '.',
-                (line[ 2] >= PRINTABLE_MIN && line[ 2] <= PRINTABLE_MAX) ? line[ 2] : '.',
-                (line[ 3] >= PRINTABLE_MIN && line[ 3] <= PRINTABLE_MAX) ? line[ 3] : '.',
-                (line[ 4] >= PRINTABLE_MIN && line[ 4] <= PRINTABLE_MAX) ? line[ 4] : '.',
-                (line[ 5] >= PRINTABLE_MIN && line[ 5] <= PRINTABLE_MAX) ? line[ 5] : '.',
-                (line[ 6] >= PRINTABLE_MIN && line[ 6] <= PRINTABLE_MAX) ? line[ 6] : '.',
-                (line[ 7] >= PRINTABLE_MIN && line[ 7] <= PRINTABLE_MAX) ? line[ 7] : '.',
-                (line[ 8] >= PRINTABLE_MIN && line[ 8] <= PRINTABLE_MAX) ? line[ 8] : '.',
-                (line[ 9] >= PRINTABLE_MIN && line[ 9] <= PRINTABLE_MAX) ? line[ 9] : '.',
-                (line[10] >= PRINTABLE_MIN && line[10] <= PRINTABLE_MAX) ? line[10] : '.',
-                (line[11] >= PRINTABLE_MIN && line[11] <= PRINTABLE_MAX) ? line[11] : '.',
-                (line[12] >= PRINTABLE_MIN && line[12] <= PRINTABLE_MAX) ? line[12] : '.',
-                (line[13] >= PRINTABLE_MIN && line[13] <= PRINTABLE_MAX) ? line[13] : '.',
-                (line[14] >= PRINTABLE_MIN && line[14] <= PRINTABLE_MAX) ? line[14] : '.',
-                (line[15] >= PRINTABLE_MIN && line[15] <= PRINTABLE_MAX) ? line[15] : '.'
-                );
+    for (int i = line_cursor; i < BYTES_PER_LINE; i++)
+    {
+        line[i] = 0;
     }
+    printf(
+        "%.*x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+        LINE_COUNTER_WIDTH, line_count,
+        line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
+        line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15],
+        (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
+        (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
+        (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
+        (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
+        (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
+        (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
+        (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
+        (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
+        (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
+        (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
+        (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
+        (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
+        (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
+        (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
+        (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
+        (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.');
 
-
-    
     fclose(input_file);
     return 0;
 }
