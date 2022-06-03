7410dec1bcab9a3f23580fa43bb56f1c05de3e58
Deven Blake <trinity@trinity.moe>
2022-06-03 19:30:27 -0400

    use ctype.h instead of macros, print_sidebar, use stdbool.h instead of macros

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,3 +1,5 @@
+#include<ctype.h>
+#include<stdbool.h>
 #include<stdio.h>
 #include<stdint.h>
 #include<sys/stat.h>
@@ -6,11 +8,22 @@
 #define LINE_COUNTER_WIDTH   8
 #define BYTES_PER_LINE       16
 
-#define PRINTABLE_MIN_ASCII  32
-#define PRINTABLE_MAX_ASCII  126    // https://en.wikipedia.org/wiki/ASCII#Printable_characters
-
 #define PRINTF_BUFFER_SIZE   3200
 
+/* Prints out the ASCII-view sidebar in canonical mode
+ * e.g. |hello..| for 'h','e','l','l','o','\r','\n'.
+ * Does not include a trailing newline. */
+void print_sidebar(FILE *output_stream, char *line, int line_len)
+{
+    int i;
+
+    fputc('|', output_stream);
+    for(i = 0; i < line_len; ++i){
+        fputc(isprint(line[i]) ? line[i] : '.', output_stream);
+    }
+    fputc('|', output_stream);
+}
+    
 int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,char *output_file_name)
 {
     FILE *input_stream;
@@ -89,7 +102,7 @@ int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,cha
 
     if(byte_cursor % BYTES_PER_LINE != 0)
     {
-        int stop_execution = FALSE;
+        int stop_execution = false;
 
         for(uint64_t i = 0; i < BYTES_PER_LINE; i++)
         {
@@ -103,39 +116,27 @@ int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,cha
 
             if(line[i] == EOF || no_of_bytes == 0)
             {
-                stop_execution = TRUE;
+                stop_execution = true;
                 break;
             }       
         }
 
         fprintf(output_stream,
-                "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+                "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  ",
                 LINE_COUNTER_WIDTH, line_count,
                 line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],
-                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],
-                (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
-                (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
-                (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
-                (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
-                (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
-                (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
-                (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
-                (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
-                (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
-                (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
-                (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
-                (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
-                (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
-                (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
-                (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
-                (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.'
-                );   
-                line_count++;
+                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15]
+               	);
 
-                if(stop_execution)
-                {
-                    return 0;
-                }
+        print_sidebar(output_stream, line, 16);
+        fputc('\n', output_stream);
+
+        line_count++;
+
+        if(stop_execution)
+        {
+            return 0;
+        }
     }
 
     while( (int_byte = fgetc(input_stream)) != EOF && no_of_bytes != 0)
@@ -143,34 +144,20 @@ int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,cha
         if( line_cursor == BYTES_PER_LINE )
         {
             fprintf(output_stream,
-                "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+                "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  ",
                 LINE_COUNTER_WIDTH, line_count,
                 line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],
-                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15],
-                (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
-                (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
-                (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
-                (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
-                (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
-                (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
-                (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
-                (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
-                (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
-                (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
-                (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
-                (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
-                (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
-                (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
-                (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
-                (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.'
+                line[8],line[9],line[10],line[11],line[12],line[13],line[14],line[15]
                 );
+            print_sidebar(output_stream, line, 16);
+            fputc('\n', output_stream);
 
-                line_count  += 16;
-                line_cursor  =  0;
+            line_count  += 16;
+            line_cursor  =  0;
 
-                line[line_cursor] = int_byte;
-                no_of_bytes--;
-                line_cursor++;
+            line[line_cursor] = int_byte;
+            no_of_bytes--;
+            line_cursor++;
         }
         else
         {
@@ -188,26 +175,12 @@ int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,cha
         line[i] = 0;
     }
     fprintf(output_stream,
-        "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+        "%.*llx  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x  ",
         LINE_COUNTER_WIDTH, line_count,
         line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
-        line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15],
-        (line[ 0] >= PRINTABLE_MIN_ASCII && line[ 0] <= PRINTABLE_MAX_ASCII) ? line[ 0] : '.',
-        (line[ 1] >= PRINTABLE_MIN_ASCII && line[ 1] <= PRINTABLE_MAX_ASCII) ? line[ 1] : '.',
-        (line[ 2] >= PRINTABLE_MIN_ASCII && line[ 2] <= PRINTABLE_MAX_ASCII) ? line[ 2] : '.',
-        (line[ 3] >= PRINTABLE_MIN_ASCII && line[ 3] <= PRINTABLE_MAX_ASCII) ? line[ 3] : '.',
-        (line[ 4] >= PRINTABLE_MIN_ASCII && line[ 4] <= PRINTABLE_MAX_ASCII) ? line[ 4] : '.',
-        (line[ 5] >= PRINTABLE_MIN_ASCII && line[ 5] <= PRINTABLE_MAX_ASCII) ? line[ 5] : '.',
-        (line[ 6] >= PRINTABLE_MIN_ASCII && line[ 6] <= PRINTABLE_MAX_ASCII) ? line[ 6] : '.',
-        (line[ 7] >= PRINTABLE_MIN_ASCII && line[ 7] <= PRINTABLE_MAX_ASCII) ? line[ 7] : '.',
-        (line[ 8] >= PRINTABLE_MIN_ASCII && line[ 8] <= PRINTABLE_MAX_ASCII) ? line[ 8] : '.',
-        (line[ 9] >= PRINTABLE_MIN_ASCII && line[ 9] <= PRINTABLE_MAX_ASCII) ? line[ 9] : '.',
-        (line[10] >= PRINTABLE_MIN_ASCII && line[10] <= PRINTABLE_MAX_ASCII) ? line[10] : '.',
-        (line[11] >= PRINTABLE_MIN_ASCII && line[11] <= PRINTABLE_MAX_ASCII) ? line[11] : '.',
-        (line[12] >= PRINTABLE_MIN_ASCII && line[12] <= PRINTABLE_MAX_ASCII) ? line[12] : '.',
-        (line[13] >= PRINTABLE_MIN_ASCII && line[13] <= PRINTABLE_MAX_ASCII) ? line[13] : '.',
-        (line[14] >= PRINTABLE_MIN_ASCII && line[14] <= PRINTABLE_MAX_ASCII) ? line[14] : '.',
-        (line[15] >= PRINTABLE_MIN_ASCII && line[15] <= PRINTABLE_MAX_ASCII) ? line[15] : '.');
+        line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15]);
+    print_sidebar(output_stream, line, 16);
+    fputc('\n', output_stream);
 
     fclose(input_stream);
     fclose(output_stream);
--- src/hexdump.h
+++ src/hexdump.h
@@ -3,9 +3,6 @@
 
 #define HEXDUMP_VERSION "0.1.0"
 
-#define FALSE 0
-#define TRUE  1
-
 int print_hex(char *input_file_name,uint64_t start_byte,uint64_t no_of_bytes,char *output_file_name);
 
 #endif
