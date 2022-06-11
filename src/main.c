26fe01697c8834f4b9fe68614148e4192184fe3b
Acedev003 <acedeveloper003@gmail.com>
2022-06-12 01:00:00 +0530

    Implement CLI argument parsing via getopt

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<limits.h>
 #include<stdint.h>
-#include<errno.h>
+#include<getopt.h>
+#include<stdbool.h>
 #include "hexdump.h"
+
+void print_usage(char *program_name);
 
 int main(int argc,char* argv[])
 {
@@ -12,64 +13,59 @@ int main(int argc,char* argv[])
     uint64_t start_byte  = 0;
     uint64_t no_of_bytes = 0;
 
-    int save_to_file = FALSE;
-    int file_name_index = 0;
-
+    int  file_name_index = 0;
+    bool save_to_file    = false;
+    
     char *number_end_ptr;
 
-    if(argc % 2 != 0)
+    int option;
+    while((option = getopt(argc,argv,"hs:n:f:")) != -1)
     {
-        fprintf(stderr,"\nIncomplete Parameters. Type [%s -h] for all available options.\n",argv[0]);
-        return -1;
+        switch(option)
+        {
+            case 'h' : print_usage(argv[0]);
+                       return EXIT_SUCCESS;
+
+            case 's' : start_byte = strtoull(optarg,&number_end_ptr,10);
+                       if(number_end_ptr == optarg || *number_end_ptr != '\0')
+                       { 
+                           fprintf(stderr,"\nError: Failed to parse -s value");
+                           return EXIT_FAILURE;
+                       }
+                       if((start_byte == UINT64_MAX) && (errno == ERANGE))
+                       {
+                           fprintf(stderr,"\nError: Input value (-s) out of range");
+                           return EXIT_FAILURE;
+                       }
+                       break;
+
+            case 'n' : no_of_bytes = strtoull(optarg,&number_end_ptr,10);
+                       if (number_end_ptr == optarg || *number_end_ptr != '\0')
+                       {
+                           fprintf(stderr, "\nError: Failed to parse -n value");
+                           return EXIT_FAILURE;
+                       }
+                       if ((start_byte == UINT64_MAX) && (errno == ERANGE))
+                       {
+                           fprintf(stderr, "\nError: Input value (-n) out of range");
+                           return EXIT_FAILURE;
+                       }
+                       break;
+
+            case 'f' : save_to_file    = true;
+                       file_name_index = optind - 1;
+                       break;
+
+            default  : print_usage(argv[0]);
+                       return EXIT_FAILURE;
+        }
     }
 
-
-    for(int i = 1; i < argc; i+=2)
+    if(optind + 1 != argc)
     {
-        if((argv[i][0] == '-') && (strlen(argv[i]) == 2))
-        {
-            switch(argv[i][1])
-            {
-                case 'h': printf("Usage:\n\n");
-                          printf("   %s [options] <file_name>\n\n",argv[0]);
-                          printf("Options:\n\n");
-                          printf(" -s: skips specified amount of bytes (Default value: 0)\n");
-                          printf(" -n: interpret only 'n' bytes of input (Default value: 0) [Prints entire file if value is 0 or below]\n");
-                          printf(" -f: Saves output into the specified file\n");
-                          printf(" -h: View help\n");
-                          return 0;
-
-                case 's': start_byte = strtoull(argv[i+1],&number_end_ptr,10);
-                          if(number_end_ptr == argv[i+1] || *number_end_ptr != '\0')
-                          { 
-                              fprintf(stderr,"\nError: Failed to parse -s value");
-                              return -1;
-                          }
-                          if((start_byte == UINT64_MAX) && (errno == ERANGE))
-                          {
-                              fprintf(stderr,"\nError: Input value (-s) out of range");
-                              return -1;
-                          }
-                          break;
-
-                case 'n': no_of_bytes = strtoull(argv[i+1],&number_end_ptr,10);
-                          if(number_end_ptr == argv[i+1] || *number_end_ptr != '\0')
-                          {
-                              fprintf(stderr,"\nError: Failed to parse -n value");
-                              return -1;
-                          }
-                          if((start_byte == UINT64_MAX) && (errno == ERANGE))
-                          {
-                              fprintf(stderr,"\nError: Input value (-n) out of range");
-                              return -1;
-                          }
-                          break;
-                          
-                case 'f': save_to_file    = TRUE;
-                          file_name_index = i + 1;
-                          break;
-            }
-        }
+        printf("Error: Arguments in wrong format\n");
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
     }
 
     if(!save_to_file)
@@ -81,5 +77,16 @@ int main(int argc,char* argv[])
         print_hex(argv[argc-1],start_byte,no_of_bytes,argv[file_name_index]);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+
+void print_usage(char *program_name)
+{
+    printf("\nUsage: %s [options] filename\n\n",program_name);
+    printf("Options:\n");
+    printf(" -s: skips specified amount of bytes (Default value: 0)\n");
+    printf(" -n: interpret only 'n' bytes of input (Default value: 0) [Prints entire file if value is 0 or below]\n");
+    printf(" -f: Saves output into the specified file\n");
+    printf(" -h: View help\n");
 }
